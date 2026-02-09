05afa2d37b48aa15e530981a55d5b257cc902b23
Krm5UT <krm4672@my.utexas.edu>
2026-02-09 10:34:31 -0600

    added boy

--- js/kitchen.js
+++ js/kitchen.js
@@ -1,18 +1,60 @@
 // Room functionality for Storytelling Website
 
 let kitchenImg;
+let boyHappyImg, boyWorriedImg, boySadImg;
+let currentBoyImg;
+let boyClickCount = 0;
+
+// Boy size and position (adjust boyBaseHeight to change size)
+let boyBaseHeight = 300; // Adjust this value to change boy size
+let boyHappyWidth, boyHappyHeight;
+let boyWorriedWidth, boyWorriedHeight;
+let boySadWidth, boySadHeight;
+let boyWidth = 0;
+let boyHeight = 0;
+let boyX = 0;
+let boyY = 0;
 
 function preload() {
   // Load the room image before setup runs
   kitchenImg = loadImage('../images/kitchen.jpg');
+  boyHappyImg = loadImage('../images/boy.happy.png');
+  boyWorriedImg = loadImage('../images/boy.worried.png');
+  boySadImg = loadImage('../images/boy.sad.png');
 }
 
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
+  
+  // Calculate dimensions for each boy image based on their own aspect ratio
+  let happyAspect = boyHappyImg.width / boyHappyImg.height;
+  boyHappyHeight = boyBaseHeight;
+  boyHappyWidth = boyHappyHeight * happyAspect;
+  
+  let worriedAspect = boyWorriedImg.width / boyWorriedImg.height;
+  boyWorriedHeight = boyBaseHeight;
+  boyWorriedWidth = boyWorriedHeight * worriedAspect;
+  
+  let sadAspect = boySadImg.width / boySadImg.height;
+  boySadHeight = boyBaseHeight;
+  boySadWidth = boySadHeight * sadAspect;
+  
+  // Start with happy boy
+  currentBoyImg = boyHappyImg;
+  boyWidth = boyHappyWidth;
+  boyHeight = boyHappyHeight;
+  
+  // Position boy (centered horizontally, bottom of screen)
+  boyX = (width - boyWidth) / 2;
+  boyY = height - boyHeight - 50;
 }
 
 function windowResized() {
   resizeCanvas(window.innerWidth, window.innerHeight);
+  
+  // Reposition boy when window resizes
+  boyX = (width - boyWidth) / 2;
+  boyY = height - boyHeight - 50;
 }
 
 function draw() {
@@ -39,6 +81,43 @@ function draw() {
     drawY = 0;
   }
   
-  // Display the room image centered and scaled
+  // Display the kitchen image centered and scaled
   image(kitchenImg, drawX, drawY, drawWidth, drawHeight);
+  
+  // Draw boy image ON TOP of kitchen
+  image(currentBoyImg, boyX, boyY, boyWidth, boyHeight);
+  
+  // Check if mouse is hovering over boy
+  let isHoveringBoy = mouseX > boyX && mouseX < boyX + boyWidth &&
+                      mouseY > boyY && mouseY < boyY + boyHeight;
+  
+  cursor(isHoveringBoy ? 'pointer' : 'default');
+}
+
+function mousePressed() {
+  // Check if boy was clicked
+  if (mouseX > boyX && mouseX < boyX + boyWidth &&
+      mouseY > boyY && mouseY < boyY + boyHeight) {
+    
+    boyClickCount++;
+    
+    // First click: change to worried
+    if (boyClickCount === 1) {
+      currentBoyImg = boyWorriedImg;
+      boyWidth = boyWorriedWidth;
+      boyHeight = boyWorriedHeight;
+      // Recenter after size change
+      boyX = (width - boyWidth) / 2;
+      boyY = height - boyHeight - 50;
+    }
+    // Second click: change to sad
+    else if (boyClickCount === 2) {
+      currentBoyImg = boySadImg;
+      boyWidth = boySadWidth;
+      boyHeight = boySadHeight;
+      // Recenter after size change
+      boyX = (width - boyWidth) / 2;
+      boyY = height - boyHeight - 50;
+    }
+  }
 }
